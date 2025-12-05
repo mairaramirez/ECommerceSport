9dd4f6e07404fafb5670e29db590ca626439c7a9
mairaramirez <mairaramirez9898@gmail.com>
2025-12-05 17:11:12 -0300

    editamos el env

--- Backend/servicio/upload.js
+++ Backend/servicio/upload.js
@@ -2,7 +2,7 @@ class Servicio {
     constructor() {}
 
      recibirArchivo = async file => {
-        const baseUrl = process.env.BASE_URL || 'http://localhost:8080'
+        const baseUrl = process.env.BASE || 'http://localhost:8080'
         const urlFoto = `${baseUrl}/uploads/${file.filename}`
         return urlFoto
     } 
